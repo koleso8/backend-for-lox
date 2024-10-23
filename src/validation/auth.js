5aab783149f14090883907621743a7561d33a16d
koleso8 <dev.koleso@gmail.com>
2024-10-23 12:21:34 +0300

    Update auth.js

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
-  city: Joi.string().required(),
+  city: Joi.string(),
   exp: Joi.number(),
   stream: Joi.string(),
   tel: Joi.string(),
